2ef979ea565dd4e70a65a0910b02de5fd901b0a4
Suprasanna Bhaumik <suprasanna.bhaumik86@gmail.com>
2022-03-24 09:27:38 +0530

    optional imports organized

--- src/com/learning/java8/streams/optional/Optional_Map_Flatmap.java
+++ src/com/learning/java8/streams/optional/Optional_Map_Flatmap.java
@@ -4,8 +4,6 @@ import com.learning.java8.Util;
 import com.learning.model.Bookmark;
 import com.learning.model.Note;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class Optional_Map_Flatmap {
